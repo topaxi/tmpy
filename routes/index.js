4184b2b8293b90cf4873cb5ddb2729c6b4dec7da
Damian Senn <damian.senn@gmail.com>
2013-03-07 16:57:04 +0100

    view images and text files inline in browsers

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
-var File   = require('../models/file')
-  , fs     = require('fs')
-  , path   = require('path')
-  , config = require('../config.json')
+var File     = require('../models/file')
+  , fs       = require('fs')
+  , path     = require('path')
+  , config   = require('../config.json')
+  , basename = path.basename
 
 module.exports = function(app) {
   app.get('/', function(req, res) {
@@ -14,7 +15,7 @@ module.exports = function(app) {
     file.name = req.files.file.name
     file.size = req.files.file.size
     file.type = req.files.file.type
-    file.hash = path.basename(req.files.file.path)
+    file.hash = basename(req.files.file.path)
     file.date = new Date
 
     file.save(function(err) {
@@ -26,7 +27,15 @@ module.exports = function(app) {
 
   app.get('/uploads/:id', function(req, res, next) {
     File.findOne({ 'hash': req.params.id }, function(err, file) {
-      res.download(file.path, file.name)
+      // Try to view images and text inline
+      if (file.type == 'text/plain' || /^image\//.test(file.type)) {
+        res.type(file.type)
+        res.set('Content-Disposition', 'inline; filename="'+ basename(file.name) +'"')
+        res.sendfile(file.path)
+      }
+      else {
+        res.download(file.path, file.name)
+      }
     })
   })
 }
