39093805dda88b2cc417fa442cf5889f79f708b1
Thomas Philipona <philipona@puzzle.ch>
2016-10-10 15:25:14 +0200

    Made ip configurable

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const config  = require('./lib/config')
 const app = express()
 
 app.set('port', config.port)
+app.set('ip', config.ip)
 app.set('views', './views')
 app.set('view engine', 'pug')
 app.use(morgan(config.logger))
@@ -16,6 +17,6 @@ app.use('/js', express.static('./node_modules/clipboard/dist'))
 require('./routes')(app)
 require('./lib/janitor')
 
-http.createServer(app).listen(app.get('port'), () => {
-  console.log(`Express server listening on port ${app.get('port')}`)
+http.createServer(app).listen(app.get('port'), app.get('ip'), () => {
+  console.log(`Express server listening on port ${app.get('port')} and ip ${app.get('ip')}`)
 })
