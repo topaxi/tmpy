168b7f41d39979fef241e030790b186bf90db2d4
Thomas Philipona <philipona@puzzle.ch>
2016-10-10 15:38:34 +0200

    configure database connection via envs

--- schemas/index.js
+++ schemas/index.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose')
 const config   = require('../lib/config')
 
 mongoose.Promise = global.Promise
-mongoose.connect(config.db.host, config.db.name)
+mongoose.connect(`mongodb://${config.db.user}:${config.db.password}@${config.db.host}/${config.db.name}`);
 
 exports.FileSchema = require('./file')
